404f6db7b623c7d7522e6be4ff8f56c0921967b6
Amal Boukili <boukili.amal@gmail.com>
2018-11-14 22:41:08 +0000

    Stocker, obtenir et ajouter des produits

--- src/Article.java
+++ src/Article.java
@@ -2,6 +2,7 @@ public class Article{
 	private String titre;
 	private double prix;
 	private int quantité;
+	private Produit [] produits;
 	
 	
 	//Constructeurs
@@ -10,10 +11,59 @@ public class Article{
 		this.titre=titre;
 		this.prix= prix;
 		this.quantité=quantité;
+		this.produits= new Produit[100];
+		
 		
 	}
 	
+	//strocker les produits
+	//avant le stockage on cherche dans le tableau Produit l'index d'élement vide
+	public int getIndexProduits() {
+		int index = 0;
+		for(int i=0; i<this.produits.length; i++){
+			
+			if(this.produits[i] == null) {
+				index = i;
+				break;
+			}
+		}
+		return index;
+	}
+	
+	public void stockerProduit(String titre) {
+		int index = this.getIndexProduits();
+		this.produits[index] = new Produit(titre);
+		}
+	
+	
+	//Obtenir les produits
+	public void getProduis() {
+		System.out.println("Artcile Courant : " + this.titre);
+		for(int i=0; i<this.produits.length; i++){
+			if(this.produits[i] != null) {
+				System.out.println(this.produits[i].getTitre());
+			}
+		}
+    }
+	
+	
+	//Ajouter des produits
+	public void AjouterProduits(String [] titre) {
+		int index = this.getIndexProduits();
+		for(int i=0; i<titre.length; i++) {
+			produits[index] = new Produit(titre[i]);
+			index ++;			
+		}
+		
+		
+		
+	}
 
+	
+	
+
+	
+	
 	//Les accesseurs
 	public String getTitre() {
 		return titre;
