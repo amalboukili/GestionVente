7f8382f7c498b15aab51386704dfcab3fb102d5c
Amal Boukili <boukili.amal@gmail.com>
2018-11-14 23:17:20 +0000

    Création de la class enum Promotion

---
+++ src/Promotion.java
@@ -0,0 +1,35 @@
+
+public enum Promotion {
+	ETE(0.9,"ETE"),
+	PRINTEMPS(0.7," PRINTEMPS "),
+	AUTOMNE(0.95, " AUTOMNE "),
+	HIVER(0.8, " HIVER "),
+	NOMAL(1.0, "");
+	
+	private String suffixe;
+	private double price;
+	
+	
+	Promotion(double price, String suffixe) {
+		this.price=price;
+		this.suffixe=suffixe;	
+	}
+	
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price=price;
+	}
+	
+	public String getSuffixe() {
+		return suffixe;
+	}
+	public void setPrice(String suffixe) {
+		this.suffixe=suffixe;	
+	}
+	
+
+
+}
